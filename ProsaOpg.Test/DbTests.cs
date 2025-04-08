5a0f9e16eb81fd5e4d05e2f4d813e8f847c920be
Michell Cronberg <michell@cronberg.dk>
2025-04-08 21:00:43 +0200

    Add database tests for mock data access service

---
+++ ProsaOpg.Test/DbTests.cs
@@ -0,0 +1,15 @@
+﻿using ProsaOpg.Data;
+
+namespace ProsaOpg.Test;
+
+public class DbTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var mockService = new MockDataAccessService();
+        var customers = mockService.GetAllCustomers();
+        Assert.NotNull(customers);
+        Assert.Equal(5, customers.Count());
+    }
+}
